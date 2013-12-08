87578142a1f2ab9bc41cb0c97693c524d71cfc6e
Javier Hernández Gil <bilbo@deverbena.com>
2013-12-08 21:11:14 +0100

    Bundle init

---
+++ JhgBootstrapBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jhg\BoostrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ * @author Javi Hernández Gil <bilbo@deverbena.com>
+ */
+class JhgBootstrapBundle extends Bundle
+{
+	
+}
